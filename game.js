72f80c2066a8a854f682311a5183da1ba4f6e340
laulexa <laura.alexandra.roa@gmail.com>
2022-09-06 22:37:03 -0700

    adding styles tot he text inside the canvas

--- game.js
+++ game.js
@@ -4,6 +4,11 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', startGame);
 
 function startGame() {
-    game.fillRect(0,0,100,100);
-    game.clearRect(0,0,50,50)
+    // game.fillRect(0,0,100,100);
+    // game.clearRect(50,50,50,50);
+    // game.clearRect(0,0,50,100);
+    game.font = '25px  Verdana';
+    game.fillStyle = 'purple';
+    game.textAlign = 'left';
+    game.fillText('Hello', 50,50);
 }
