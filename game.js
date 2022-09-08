58920cfd14f4af2a1a1b6503662140cd8d2ca60f
laulexa <laura.alexandra.roa@gmail.com>
2022-09-07 20:52:40 -0700

    adding attributes to the canvas

--- game.js
+++ game.js
@@ -1,14 +1,36 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  elementsSize = canvasSize / 10;
+
+  startGame();
+}
 
 function startGame() {
-    // game.fillRect(0,0,100,100);
-    // game.clearRect(50,50,50,50);
-    // game.clearRect(0,0,50,100);
-    game.font = '25px  Verdana';
-    game.fillStyle = 'purple';
-    game.textAlign = 'left';
-    game.fillText('Hello', 50,50);
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let row = 1; row <= 10; row++) {
+      for (let col = 1; col <= 10; col++) {
+        game.fillText(emojis['X'], elementsSize * col , elementsSize * row);
+    } 
+  }
 }
